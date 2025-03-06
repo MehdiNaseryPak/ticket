ac23e303c1927aeb4edece71ba9c7df7adf2af01
MehdiNaseryPak <mhdynasrypak@gmail.com>
2025-03-07 02:05:33 +0330

    create admin sidebar and link to route

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -3,6 +3,26 @@
         <ul>
             <li data-toggle="tooltip" title="دسته بندی ها">
                 <a href="#categories" title=" دسته بندی ها">
+                    <i class="icon ti-layout-list-thumb-alt"></i>
+                </a>
+            </li>
+            <li data-toggle="tooltip" title="اسلایدر ها">
+                <a href="#sliders" title="اسلایدر ها">
+                    <i class="icon ti-image"></i>
+                </a>
+            </li>
+            <li data-toggle="tooltip" title="جایگاه ها">
+                <a href="#positions" title="جایگاه ها">
+                    <i class="icon ti-layers"></i>
+                </a>
+            </li>
+            <li data-toggle="tooltip" title="فیلم ها">
+                <a href="#movies" title="فیلم ها">
+                    <i class="icon ti-video-clapper"></i>
+                </a>
+            </li>
+            <li data-toggle="tooltip" title="کاربران">
+                <a href="#users" title="کاربران">
                     <i class="icon ti-user"></i>
                 </a>
             </li>
@@ -25,8 +45,44 @@
             <li>
                 <a href="#">دسته بندی</a>
                 <ul>
-                    <li><a href="#">ایجاد دسته بندی</a></li>
-                    <li><a href="#">لیست دسته بندی ها</a></li>
+                    <li><a href="{{ route('admin.categories.create') }}">ایجاد دسته بندی</a></li>
+                    <li><a href="{{ route('admin.categories.index') }}">لیست دسته بندی ها</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul id="sliders">
+            <li>
+                <a href="#">اسلایدر</a>
+                <ul>
+                    <li><a href="{{ route('admin.sliders.create') }}">ایجاد اسلایدر</a></li>
+                    <li><a href="{{ route('admin.sliders.index') }}">لیست اسلایدر ها</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul id="positions">
+            <li>
+                <a href="#">جایگاه ها</a>
+                <ul>
+                    <li><a href="{{ route('admin.positions.create') }}">ایجاد جایگاه</a></li>
+                    <li><a href="{{ route('admin.positions.index') }}">لیست جایگاه ها</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul id="movies">
+            <li>
+                <a href="#">فیلم</a>
+                <ul>
+                    <li><a href="#">ایجاد فیلم</a></li>
+                    <li><a href="#">لیست فیلم ها</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul id="users">
+            <li>
+                <a href="#">کاربران</a>
+                <ul>
+                    <li><a href="{{ route('admin.users.create') }}">ایجاد کاربر</a></li>
+                    <li><a href="{{ route('admin.users.index') }}">لیست کاربران</a></li>
                 </ul>
             </li>
         </ul>
--- resources/views/admin/layouts/script.blade.php
+++ resources/views/admin/layouts/script.blade.php
@@ -1,4 +1,4 @@
-<script src="{{ asset('admin-assets/vendors/bundle.') }}"></script>
+<script src="{{ asset('admin-assets/vendors/bundle.js') }}"></script>
 <script src="{{ asset('admin-assets/vendors/slick/slick.min.js') }}"></script>
 <script src="{{ asset('admin-assets/vendors/vmap/jquery.vmap.min.js') }}"></script>
 <script src="{{ asset('admin-assets/vendors/sweetalert2/sweetalert2.all.min.js') }}"></script>
